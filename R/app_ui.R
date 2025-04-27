e10700e6416ace641c7ea6253172b21bcf23d638
lionelsaxer <lionel.saxer@gmail.com>
2025-04-27 21:26:24 +0200

    define ui of the app and add ui of zeitreihen module

--- R/app_ui.R
+++ R/app_ui.R
@@ -5,12 +5,20 @@
 #' @import shiny
 #' @noRd
 app_ui <- function(request) {
-  tagList(
+  shiny::tagList(
     # Leave this function for adding external resources
-    golem_add_external_resources(),
+    # golem_add_external_resources(),
     # Your application UI logic
-    fluidPage(
-      golem::golem_welcome_page() # Remove this line to start building your UI
+    bslib::page_navbar(
+      theme = bslib::bs_theme(version = 5, bootswatch = "lux"),
+      title = "KoSig Dashboard",
+
+      bslib::navset_card_pill(
+        bslib::nav_panel(
+          title = "Zeitreihen",
+          mod_zeitreihen_ui("zeitreihen")
+        )
+      )
     )
   )
 }
